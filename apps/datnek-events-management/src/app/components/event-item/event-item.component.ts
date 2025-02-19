f165a05cfbb8a686852860763c075c5e48ee42ef
djery-tom <dietchidjery@gmail.com>
2025-02-19 20:55:25 +0100

    ui: implements events list

--- apps/datnek-events-management/src/app/components/event-item/event-item.component.ts
+++ apps/datnek-events-management/src/app/components/event-item/event-item.component.ts
@@ -1,15 +1,23 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventOutput } from '@datnek-events-management/events';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-event-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslatePipe],
   templateUrl: './event-item.component.html',
   styleUrl: './event-item.component.css',
 })
 export class EventItemComponent {
-
   @Input() event!: EventOutput.Get;
+
+  onDelete() {
+
+  }
+
+  onUpdate() {
+
+  }
 }
